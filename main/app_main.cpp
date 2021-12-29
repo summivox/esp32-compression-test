284bfcf86d991698116dfc5ae0be4930cc9b8749
summivox <summivox@gmail.com>
2021-12-29 13:30:17 -0800

    refactor out completed dir iter

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -39,47 +39,6 @@ esp_err_t SetupSdCard() {
   return g_sd_card ? ESP_OK : ESP_FAIL;
 }
 
-////////////////////////////////////////////////////////
-
-extern "C" {
-#include <dirent.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-}
-#include <optional>
-
-class VfsDirIterImpl {
- public:
-  using Item = dirent*;
-  VfsDirIterImpl() = default;
-  VfsDirIterImpl(const char* path) : dir_(opendir(path)) {}
-  ~VfsDirIterImpl() {
-    if (dir_) {
-      (void)closedir(dir_);
-    }
-  }
-
-  std::optional<Item> Next() {
-    if (!dir_) {
-      return std::nullopt;
-    }
-    dirent* p = readdir(dir_);
-    if (p) {
-      return p;
-    }
-    return std::nullopt;
-  }
-
- private:
-  DIR* dir_;
-};
-class VfsDirIter : public RustIter<VfsDirIterImpl> {
- public:
-  explicit VfsDirIter(const char* path) : RustIter<VfsDirIterImpl>(path) {}
-};
-
-////////////////////////////////////////////////////////
-
 const std::map<uint8_t, std::string> kDirentTypeName{
     {DT_REG, "DT_REG"},
     {DT_DIR, "DT_DIR"},
@@ -89,12 +48,12 @@ const std::map<uint8_t, std::string> kDirentTypeName{
 void Test() {
   std::string dir = "/s/BD2AA7C0/355/0";
   // std::string dir = "/s/maps";
-  for (dirent* p : VfsDirIter(dir.c_str())) {
+  for (dirent* p : io::DirIter(dir.c_str())) {
     print("name=\'{}\' type={} ({})", p->d_name, p->d_type, kDirentTypeName.at(p->d_type));
     if (p->d_type == DT_REG) {
       struct stat s {};
       stat(format("{}/{}", dir, p->d_name).c_str(), &s);
-      TimeZulu mt = ToZulu(s.st_mtim);
+      TimeParts mt = ToParts(s.st_mtim);
       // st_ctim is not populated, and we don't have st_birthtim / st_birthtimespec
       print(" size={} mtim={:%F_%T}", s.st_size, mt);
     }
