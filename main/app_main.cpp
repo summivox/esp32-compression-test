476600984b134f3ca9eb3f51987ad92d58049607
summivox <summivox@gmail.com>
2022-05-28 23:42:38 -0700

    automate console command definition and registration

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -8,6 +8,8 @@
 #include "esp_err.h"
 #include "scope_guard/scope_guard.hpp"
 
+#include "common/command.hpp"
+#include "common/command_registry.hpp"
 #include "common/macros.hpp"
 #include "io/fs_utils.hpp"
 #include "io/sd_card_daemon.hpp"
@@ -40,20 +42,19 @@ const std::map<uint8_t, std::string> kDirentTypeName{
     {DT_UNKNOWN, "DT_UNKNOWN"},
 };
 
-struct ListCommandArgs {
-  arg_str* path = arg_str1(nullptr, nullptr, "<path>", "path to list");
-  struct arg_end* end = arg_end(1);
-} FLAGS_list;
-int ListCommand(int argc, char** argv) {
-  const int num_errors = arg_parse(argc, argv, (void**)&FLAGS_list);
-  if (num_errors > 0) {
-    arg_print_errors(stderr, FLAGS_list.end, argv[0]);
-    return 0;
-  }
-  CHECK(FLAGS_list.path->count == 1);
+DEFINE_COMMAND(
+    ls,
+    "list all directories and files under given path",
+    /*hint*/ nullptr,
+    {
+      arg_lit* time = arg_lit0("t", "time", "show time");
+      arg_str* path = arg_str1(nullptr, nullptr, "<path>", "path to list");
+    },
+    /*num_end*/ 1) {
+  CHECK(path->count == 1);
 
   std::string dir = "/s";
-  dir += FLAGS_list.path->sval[0];
+  dir += path->sval[0];
 
   for (dirent* p : io::DirIter(dir.c_str())) {
     printf("name='%s' type=%d (%s)", p->d_name, p->d_type, kDirentTypeName.at(p->d_type).c_str());
@@ -61,43 +62,38 @@ int ListCommand(int argc, char** argv) {
       struct stat s {};
       stat((dir + "/" + p->d_name).c_str(), &s);
       TimeParts mt = ToParts(s.st_mtim);
-      // st_ctim is not populated, and we don't have st_birthtim / st_birthtimespec
-      printf(
-          " size=%d mtim=%04d-%02d-%02d_%02d:%02d:%02d",
-          (int)s.st_size,
-          mt.tm_year + 1900,
-          mt.tm_mon + 1,
-          mt.tm_mday,
-          mt.tm_hour,
-          mt.tm_min,
-          mt.tm_sec);
+      printf(" size=%d", (int)s.st_size);
+      if (time->count) {
+        // st_ctim is not populated, and we don't have st_birthtim / st_birthtimespec
+        printf(
+            " mtim=%04d-%02d-%02d_%02d:%02d:%02d",
+            mt.tm_year + 1900,
+            mt.tm_mon + 1,
+            mt.tm_mday,
+            mt.tm_hour,
+            mt.tm_min,
+            mt.tm_sec);
+      }
     }
     printf("\n");
   }
 
   return 0;
 }
-void RegisterListCommand() {
-  constexpr esp_console_cmd_t cmd = {
-      .command = "ls",
-      .help = "list everything under given path",
-      .hint = nullptr,
-      .func = ListCommand,
-      .argtable = &FLAGS_list,
-  };
-  CHECK_OK(esp_console_cmd_register(&cmd));
-}
 
-extern "C" void app_main(void) {
+esp_console_repl_t* InitializeConsole() {
   esp_console_repl_t* repl = nullptr;
   esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
   repl_config.prompt = "rice>";
   esp_console_dev_uart_config_t repl_uart_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-  repl_uart_config.baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE;
-  CHECK_OK(esp_console_new_repl_uart(&repl_uart_config, &repl_config, &repl));
+  OK_OR_RETURN(esp_console_new_repl_uart(&repl_uart_config, &repl_config, &repl), nullptr);
   register_system_common();
-  RegisterListCommand();
+  OK_OR_RETURN(CommandRegistry::GetInstance()->Register(), nullptr);
+  return repl;
+}
 
+extern "C" void app_main(void) {
+  esp_console_repl_t* repl = InitializeConsole();
   printf(
       "\n\n\n\n"
       "FS playground\n\n\n\n"
@@ -109,8 +105,7 @@ extern "C" void app_main(void) {
     vTaskDelay(pdMS_TO_TICKS(1));
   }
   printf("done.\n");
-
-  CHECK_OK(esp_console_start_repl(repl));
+  esp_console_start_repl(repl);
 
   while (1) {
     vTaskDelay(pdMS_TO_TICKS(1));
---
+++ main/common/command_registry.cpp
@@ -0,0 +1,34 @@
+// Copyright 2022 summivox. All rights reserved.
+// Authors: summivox@gmail.com
+
+#include "common/command_registry.hpp"
+
+#include "esp_console.h"
+
+#include "common/macros.hpp"
+
+namespace {
+constexpr char TAG[] = "cmd_reg";
+}  // namespace
+
+CommandRegistry::CommandRegistry() {}
+
+CommandRegistry* CommandRegistry::GetInstance() {
+  static CommandRegistry* instance = new CommandRegistry();
+  return instance;
+}
+
+void CommandRegistry::AddCommand(const esp_console_cmd_t& command) { commands_.push_back(command); }
+
+esp_err_t CommandRegistry::Register() {
+  esp_err_t first_failure = ESP_OK;
+  for (const esp_console_cmd_t& command : commands_) {
+    ESP_LOGI(TAG, "registering command: %s", command.command);
+    if (const esp_err_t err = esp_console_cmd_register(&command); err != ESP_OK) {
+      ESP_LOGE(TAG, "failed to register command (%s): %s", esp_err_to_name(err), command.command);
+      first_failure = err;
+      // do not fail fast --- see if other commands can be registered or no
+    }
+  }
+  return first_failure;
+}
